8241e175c6200781e61abd0cdb6575389b324475
AndreeaIsabela <bala.andreea@yahoo.com>
2020-04-11 18:03:01 +0300

    Conditionally displaying QuizEnd component.

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,10 +17,13 @@ class Quiz extends React.Component {
 
     return (
       <div>
-        { isQuizEnd && <QuizEnd/> }
-        <QuizQuestion 
-          quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
-        />
+        { isQuizEnd ? 
+        <QuizEnd/> 
+        :<QuizQuestion 
+        quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
+      />
+      }
+        
       </div>
     )
   }
