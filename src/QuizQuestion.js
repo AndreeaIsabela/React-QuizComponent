fbddcf18c401f0579595d3ea12e6ba537396cb9f
AndreeaIsabela <bala.andreea@yahoo.com>
2020-04-11 18:51:34 +0300

    Add error mesage for incorect answer.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import QuizQuestionButton from './QuizQuestionButton';
 
@@ -7,15 +7,25 @@ class QuizQuestion extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      incorrectAnswer: false
+    }
   }
 
   handleClick(buttonText) {
-    if(this.props.quiz_question.answer === buttonText) {
+    if (this.props.quiz_question.answer === buttonText) {
+      this.setState((state) => {
+        return { incorrectAnswer: false }
+      })
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState((state) => {
+        return { incorrectAnswer: true }
+      })
     }
   }
   render() {
-    return(
+    return (
       <main>
         <section>
           <p>
@@ -25,14 +35,19 @@ class QuizQuestion extends React.Component {
         <section className="buttons">
           <ul>
             {this.props.quiz_question.answer_options.map((element, index) => {
-              return (<QuizQuestionButton 
+              return (<QuizQuestionButton
                 clickHandler={this.handleClick.bind(this)}
                 key={index}
-                button_text={element} 
-                />)
+                button_text={element}
+              />)
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer && 
+        <p className='error'>
+          Sorry, that's not right
+        </p>
+        }
       </main>
     )
   }
